fb593d3b44392cd4c58b081fc887b8dcfb22656a
dhananjay <dhananjayvalte28@gmail.com>
2020-07-15 09:37:14 +0530

    Dhananjay | T382 and T398 build

---
+++ precache-manifest.27a594b2fc4ca26ebcd83a37389b9a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c23f6cb2a32e5f40b7ff2dd9fa80da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4936d28cc5fe41fd3bc6",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "8ea82470ad280d153d9b",
+    "url": "/static/css/main.fa0afe1c.chunk.css"
+  },
+  {
+    "revision": "4936d28cc5fe41fd3bc6",
+    "url": "/static/js/2.cbfb387f.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.cbfb387f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea82470ad280d153d9b",
+    "url": "/static/js/main.9af0cc0a.chunk.js"
+  },
+  {
+    "revision": "554bb923822f04887cf3",
+    "url": "/static/js/runtime-main.bde9ab78.js"
+  }
+]);
